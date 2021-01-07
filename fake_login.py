a12cbc56546d3a5a1b9babe27e278321aa722770
AhmadNawaz23 <Ahmadnawaz149@gmail.com>
2021-01-07 22:56:37 +0400

    first commit

---
+++ fake_login.py
@@ -0,0 +1,31 @@
+import time
+
+
+username = 'AhmadNawaz'
+password = 'cattac321'
+
+
+user = input("User: ")
+Pass = input("Password: ")
+
+
+if user == username and Pass == password:
+  time.sleep(1)
+  print("Please wait")
+  time.sleep(2)
+  print("OK...")
+  time.sleep(3)
+  print("Loading.....")
+  time.sleep(4)
+  print(".....")
+  time.sleep(5)
+  print("Access Granted")
+
+elif user != username:
+  print("incorrect username")
+
+elif Pass != password:
+  print("incorrect password")
+
+else:
+  print("incorrect username and password")  
